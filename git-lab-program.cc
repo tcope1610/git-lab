3867d39833659ce6559f4da11602f7b3889f3dc6
Tracey Copeland <tc400617@ohio.edu>
2021-01-26 11:05:55 -0500

    program edited

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tracey Copeland
+ *        Date: 01/26/21
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
